737ca9faa6e5b346f408899bf3c2ee5174382d59
LiangYizhi <929483521@qq.com>
2018-12-29 14:43:24 +0800

    webpack bulid-2

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './app.css';
 import './app.scss';
+
 ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('app')
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
@@ -6,7 +8,11 @@ module.exports = {
 	entry: './src/app.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.js'
+		filename: 'js/index.js'
+	},
+	mode: 'development', // 设置mode
+	performance: { 
+		hints:false   
 	},
 	module: {
 		rules: [
@@ -33,13 +39,53 @@ module.exports = {
 			        fallback: 'style-loader',
 			        use: ['css-loader', 'sass-loader']
 		        })
-		    }
+		    },
+		    {
+				test: /\.(png|svg|jpg|gif)$/i,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192,
+							name: 'resource/[name].[ext]'
+						}
+					}
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192,
+							name:'resource/[name].[ext]'
+						}
+					}
+				]
+			}
 		]
+	},//webpack 4X 升级后取代 new webpack.optimize.CommonsChunkPlugin
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				commons: {
+			        name: 'commons',
+			        chunks: 'initial',
+			        minChunks: 2
+        		}
+			}
+		}
+	},
+	devServer: {
+		contentBase: './dist'
 	},
 	plugins: [
+		//处理HTML文件
 		new HtmlWebpackPlugin({
 			template:'./src/index.html'
 		}),
-		new ExtractTextPlugin("index.css")
+		//独立CSS文件
+		new ExtractTextPlugin("css/[name].css")
 	]
 };
